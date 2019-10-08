15176b68435d1e0e1030a7853353f0f73829930f
Tihan Pelser <tihan@labs.epiuse.com>
2019-10-08 12:28:31 +0200

    Removed tokens

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import os
+from gotomeeting_manager import gotomanager
+
+FIRSTNAME = "Thom"
+LASTNAME = "Schoff"
+EMAIL = "tihan+test@gmail.com"
+PRODUCTS = ["G2M",]
+
 if __name__ == "__main__":
-    print(os.path.dirname(__file__))
+    manager = gotomanager.Manager(consumer_secret=)
--- tests/test_gotomeeting.py
+++ tests/test_gotomeeting.py
@@ -2,11 +2,10 @@ from gotomeeting_manager import gotomanager
 
 
 def test_create_user():
-    gotomeeting = gotomanager.Manager(consumer_key="qj37k793dpfOQgGWtif35m4nSxWpbZiM", consumer_secret="D0wQWFxFMT7hQ5Qy")
-    gotomeeting.create_user(firstname="Thom", lastname="Schoff", email="tihan+test@labs.epiuse.com")
+    gotomeeting = gotomanager.Manager(consumer_key="", consumer_secret="")
+    gotomeeting.create_user(first_name="Thom", last_name="Schoff", email="tihan+test@labs.epiuse.com")
 
 
 def test_request_tokens():
-    gotomeeting = gotomanager.Manager(consumer_key="qj37k793dpfOQgGWtif35m4nSxWpbZiM",
-                                      consumer_secret="D0wQWFxFMT7hQ5Qy")
-    gotomeeting._request_tokens(auth_code="77ff1d20fdd12fb9a5c19d00d0add887")
+    gotomeeting = gotomanager.Manager(consumer_key="", consumer_secret="")
+    gotomeeting._request_tokens(auth_code="")
